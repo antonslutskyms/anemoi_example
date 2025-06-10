153b35acaa7a01fa2b74368591084d09173bdb0f
Anton <anton@slutsky.com>
2025-06-10 11:15:13 -0400

    latest successful environment

--- src/submit_training_aml.sh
+++ src/submit_training_aml.sh
@@ -4,15 +4,19 @@ OUTPUT_DIR=$2
 echo "Data path: $DATA_PATH"
 echo "Output directory: $OUTPUT_DIR"
 
+echo "--------------- ENVIRONMENT --------------------"
+env
+echo "------------------------------------------------"
+
 mkdir -p /pscratch/sd/t/timothys/anemoi-house/replay/atmosphere-subsampled
 
 ln -s $DATA_PATH /pscratch/sd/t/timothys/anemoi-house/replay/atmosphere-subsampled/p0
 ln -s $OUTPUT_DIR /pscratch/sd/t/timothys/anemoi-house/replay/atmosphere-subsampled/training-output
 
 export RANK=0
-export WORLD_RANK=0
-export GLOBAL_RANK=0
-export LOCAL_RANK=0
+export WORLD_RANK=$OMPI_COMM_WORLD_NODE_RANK
+export GLOBAL_RANK=$OMPI_COMM_WORLD_NODE_RANK
+export LOCAL_RANK=$OMPI_COMM_WORLD_NODE_RANK
 export WORLD_SIZE=1
 export ANEMOI_BASE_SEED=1000
 export SLURM_GPUS_PER_NODE=1
