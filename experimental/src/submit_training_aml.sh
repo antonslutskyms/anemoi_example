1eb8f4bc4b84738428560950cdef363125010165
Anton <anton@slutsky.com>
2025-06-17 07:22:04 -0400

    experimental change

--- experimental/src/submit_training_aml.sh
+++ experimental/src/submit_training_aml.sh
@@ -13,10 +13,10 @@ mkdir -p /pscratch/sd/t/timothys/anemoi-house/replay/atmosphere-subsampled
 ln -s $DATA_PATH /pscratch/sd/t/timothys/anemoi-house/replay/atmosphere-subsampled/p0
 ln -s $OUTPUT_DIR /pscratch/sd/t/timothys/anemoi-house/replay/atmosphere-subsampled/training-output
 
-export RANK=$OMPI_COMM_WORLD_NODE_RANK || $AZUREML_CR_NODE_RANK
-export WORLD_RANK=$OMPI_COMM_WORLD_NODE_RANK || $AZUREML_CR_NODE_RANK
-export GLOBAL_RANK=$OMPI_COMM_WORLD_NODE_RANK || $AZUREML_CR_NODE_RANK
-export LOCAL_RANK=$OMPI_COMM_WORLD_NODE_RANK || $AZUREML_CR_NODE_RANK
+export RANK=$OMPI_COMM_WORLD_NODE_RANK 
+export WORLD_RANK=$OMPI_COMM_WORLD_NODE_RANK 
+export GLOBAL_RANK=$OMPI_COMM_WORLD_NODE_RANK
+export LOCAL_RANK=$OMPI_COMM_WORLD_NODE_RANK
 export WORLD_SIZE=2
 export ANEMOI_BASE_SEED=1000
 export SLURM_GPUS_PER_NODE=1
